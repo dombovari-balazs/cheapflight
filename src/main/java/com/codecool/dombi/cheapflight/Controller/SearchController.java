15d5f0a3e54470e505e057fb226beb1d0d8bbaa2
Dombovári Balázs <dbalazs1219@gmail.com>
2019-09-02 23:04:47 +0200

    Project created. Basic, example Controller added.

--- src/main/java/com/codecool/dombi/cheapflight/Controller/SearchController.java
+++ src/main/java/com/codecool/dombi/cheapflight/Controller/SearchController.java
@@ -1,30 +1,41 @@
 package com.codecool.dombi.cheapflight.Controller;
 
-import com.squareup.okhttp.OkHttpClient;
 
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
 
 @RestController
 public class SearchController {
-    private OkHttpClient client = new OkHttpClient();
+    String url = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/FR/HUF/en-US/LOND-sky/PARI-sky/2019-09-09";
+    String[] host ={"x-rapidapi-host", "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"} ; // TODO: omg pls why do you do this to me??? :O
+    String[] key ={"x-rapidapi-key", "329dd100b5msh8d6f005986d19e8p1bc3efjsn4dea62c073fa"} ;
 
     @GetMapping("/example")
-    public Response listAllPlaces() throws IOException {
-        Request request = new Request.Builder()
-                .url("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/?query=Stockholm")
-                .get()
-                .addHeader("x-rapidapi-host", "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com")
-                .addHeader("x-rapidapi-key", "329dd100b5msh8d6f005986d19e8p1bc3efjsn4dea62c073fa")
-                .build();
+    public ResponseEntity<String> exampleQuotes2(){
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(host[0],host[1]);
+        headers.set(key[0],key[1]);
 
-        Response response = client.newCall(request).execute();
+        HttpEntity entity = new HttpEntity(headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                url, HttpMethod.GET, entity, String.class);
         return response;
+
+
     }
+
+
+
+
+
 
 
 
